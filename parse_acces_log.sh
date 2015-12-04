035229d7308e62d6efb889a2c710c8c3509f8c29
root <root@izhekova-mini.mushmul.net>
2015-12-04 22:24:51 +0200

    v2 of parse file

--- parse_acces_log.sh
+++ parse_acces_log.sh
@@ -6,30 +6,47 @@ counter=0
 good=0
 bad=0
 request_time=0
+TIME=0
+SENTMB=0
 temp_file=/home/izhekova/devops/temp_file.txt
-for hours in $(seq -w 0 59)  ; do
+for hours in $(seq -w 0 23)  ; do
 	for minutes in $(seq -w 0 59); do
-			grep "`date +%Y`:$hours:$minutes"  $log_file >> $temp_file
+			grep "$hours:$minutes"  $log_file >> $temp_file
 			while read -r line
 			do
-				CODE=`echo $line | awk -F' ' '{print $9}'`
-	      	                temp_time=`echo $line |awk '{print $NF}'`
+				CODE=`echo $line | awk -F' ' '{print $9}'`  
+	      	                temp_time=`echo $line |awk -F' ' '{print $NF}'`
+				temp_sent=`echo $line |awk '{NF--; print $NF}'`
+				temp_request=`echo $line |awk -F ' ' '{print $6}'`
 				TIME=$(( $TIME + $temp_time ))
+				
 				case $CODE in
 				200)
-					counter=$((counter+1))
 					good=$((good+1))
 				;;
 				500)	
-					counter=$((counter+1))
 					bad=$((bad+1))	
 				;;
-				esac	
+				esac
+				
+				if [ "$temp_request" == '"GET' ]; then
+                                        counter=$((counter+1))
+                                        SENTMB=$(( $SENTMB + $temp_sent ))
+                                else
+                                        continue
+                                fi	
 				done < $temp_file
-                        file_lines=`wc -l < $temp_file`
-			echo "Succsessfull request for `date +%Y`:$hours:$minutes" are $good
-			echo "BAD request for `date +%Y`:$hours:$minutes" are $bad
-			echo "Mean response time per minute" is $TIME/$file_lines
+	
+			file_lines=`wc -l < $temp_file`
+			echo "Successful requests for $hours:$minutes:" $good
+			echo "BAD requests for `date +%Y`:$hours:$minutes" are $bad
+			if [ "$file_lines" != "0" ]; then 
+				echo "Mean response time per minute: " $(( $TIME / $file_lines ))
+				echo "MB sent per minute: " $(( $(( $SENTMB / $counter )) / 1024 ))
+			else
+				continue
+			fi	
+				echo
                         echo -n "" > $temp_file
 	done					
 done									
