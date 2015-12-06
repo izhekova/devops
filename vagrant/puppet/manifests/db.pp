033036e3b1b9a392a7722bf2e8fcd4d2da609ce7
root <root@izhekova-mini.mushmul.net>
2015-12-06 12:27:11 +0200

    Add latest version for web and db servers

--- vagrant/puppet/manifests/db.pp
+++ vagrant/puppet/manifests/db.pp
@@ -2,4 +2,8 @@ class { 'mysql::server':
 	root_password           => 'devops_password',
 	remove_default_accounts => true,
 	override_options        => $override_options
-    }
+}
+
+package { 'vim-enhanced':
+  ensure => present,
+}
--- vagrant/puppet/manifests/web.pp
+++ vagrant/puppet/manifests/web.pp
@@ -1,1 +1,31 @@
 class { 'nginx': }
+
+file { '/var/www':
+  ensure => 'directory',
+}
+
+file { '/var/www/html':
+    ensure => 'directory',
+    owner   => nginx,
+    group   => nginx,
+}
+
+file { '/var/www/html/devops':
+  ensure => 'directory',
+  source => ['puppet:///modules/nginx'],
+  recurse => true,
+}
+nginx::resource::vhost { 'devops':
+  ensure               => present,
+  listen_port          => 80,
+  www_root             => '/var/www/html/devops',
+  use_default_location => true,
+  access_log           => '/var/log/nginx/devops_access.log',
+  error_log            => '/var/log/nginx/devops_error.log',
+}
+exec {"Stop FW for testing purpose":
+    command => "/sbin/service iptables stop",
+ }
+package { 'vim-enhanced':
+  ensure => present,
+}
