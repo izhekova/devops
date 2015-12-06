de2efcf659d7fce57102e8fc2dc0c6c025e588da
root <root@izhekova-mini.mushmul.net>
2015-12-06 10:11:20 +0200

    Fix typo in the URL

--- parse_acces_log.sh
+++ parse_acces_log.sh
@@ -15,6 +15,6 @@ Your choise here:" result
         2) awk '$9 == 500 || $9 == 404' $log_file | cut -d[ -f2 | cut -d] -f1 | awk -F: '{print "Bad request at " $2":"$3}' | sort -nk1 -nk2 | uniq -c ;;
         3) awk '$6 == "\"GET"' $log_file | awk -F'[ ]' '{ print $4 $NF}' | awk -F '[:]' '{ print $2":"$3 " " $NF}' |awk '{a[$1]+=$NF}END{for(i in a) print "Average " i,a[i]/1024/1024 " MB"}' | sort ;;
         4) awk '$6 == "\"GET"' $log_file | awk -F'[ ]' '{ print $4 $NF-1}' | awk -F '[:]' '{ print $2":"$3 " " $NF}' |awk '{count[$1]++;a[$1]+=$NF}END{for(i in a) print "Average " i,a[i]/1024/1024/count[i] " MB"}' | sort ;;
-        5) wget https://s3-eu-west-1.amazonaws.com/skyscanner-recruitment-resources/devops/access-log-example/c930ecf4b0a4426e619bddd8752c475ea772427db13eb92ee6a1a79b248ec0dc/access.log ;;
+        5) wget https://s3-eu-west-1.amazonaws.com/skyscanner-recruitement-resources/devops/access-log-example/c930ecf4b0a4426e619bddd8752c475ea772427db13eb92ee6a1a79b248ec0dc/access.log ;;
         *) echo "Please choose valid option" ;;
     esac
