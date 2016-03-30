2ff70d2c64b701ed7fa6bffe7351c74f3921327b
Irena Zhekova <renince@gmail.com>
2016-03-30 13:14:39 +0300

    Push new configuration

--- vagrant/puppet/manifests/mongo.pp
+++ vagrant/puppet/manifests/mongo.pp
@@ -1,9 +1,20 @@
 package { 'vim-enhanced':
     ensure => present,
 }
-class {'::mongodb::server':
-    auth => true,
-}
+
+class {'::mongodb::globals':
+	manage_package_repo => true,
+	repo_location => 'http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/',
+} ->
+
+class {'::mongodb::server': 
+	auth	=> false,
+	ensure	=> present,
+	rest	=> true
+	} ->
+
+class {'mongodb::client':}->
+
 mongodb::db { 'mongotestdb':
     user          => 'mongouser',
     password_hash => 'a15fbfca5e3a758be80ceaf42458bcd8',
