406212af225c48d35e2d08e1847c77bc6a3ea901
root <root@izhekova-mini.mushmul.net>
2016-04-17 16:34:45 +0300

    Small Fixes

--- vagrant/playbook/start.sh
+++ vagrant/playbook/start.sh
@@ -2,6 +2,6 @@
 PWD="/home/vagrant/errbit" 
 export MONGO_URL="mongodb://siteUserAdmin:admin@192.168.50.3:27017/admin"
 cd $PWD
-sudo nohup bundle exec rails server &
+nohup bundle exec rails server &
 sleep 10
 #END
